099357c89221ac2ce10fb558ab2c355d8d8eed56
CodeNinjas Computer 7 <arelerkin@gmail.com>
2022-12-09 17:06:46 -0600

    Started

---
+++ StringFirstAssignments/src/mathSolver.java
@@ -0,0 +1,10 @@
+public class mathSolver {
+    int formulate(int num){
+
+    }
+            public static void main(String[] args){
+            System.out.println("Hello, World!");
+            mathSolver 
+        }
+    
+}
