567fb60eee1c4442af3e49ab86cd9e229455ae30
CodeNinjas Computer #1 <codeninjasarlingtonheights@gmail.com>
2022-12-10 12:05:29 -0600

    worked on mathsolver

--- StringFirstAssignments/src/mathSolver.java
+++ StringFirstAssignments/src/mathSolver.java
@@ -1,10 +1,70 @@
-public class mathSolver {
-    int formulate(int num){
+import java.util.*;
 
-    }
-            public static void main(String[] args){
-            System.out.println("Hello, World!");
-            mathSolver 
+public class mathSolver {
+    int factorial(int num) {
+        int total = 1;
+        while (num > 0) {
+            total *= num;
+            num--;
         }
-    
+        return total;
+    }
+
+    void runner() {
+        // Detects char for correct calculation
+        Scanner sc = new Scanner(System.in); // System.in is a standard input stream
+        System.out.print("Enter Calcultaion Opeation - ");
+        String a = sc.next();
+        a = a.toUpperCase();
+        char b = a.toCharArray()[0];
+        // Decides what calculation is needed
+        if (b == 'P') {
+            System.out.println("Starting Permutation Calculation...");
+            System.out.print("Enter N - ");
+            int n = sc.nextInt();
+            System.out.print("Enter R - ");
+            int r = sc.nextInt();
+            System.out.println("Equation = " + n + b + r);
+            System.out.print("Continue? (Y/N) ");
+            String c = sc.next();
+            c = c.toUpperCase();
+            char f = c.toCharArray()[0];
+            if (f == 'Y') {
+                sc.close();
+                System.out.println("Locked Input");
+                final int top = factorial(n);
+                final int bottom = factorial(n-r);
+                final int solution = top/bottom;
+                System.out.println(solution);
+            }
+        }else if(b == 'C'){
+            System.out.println("Starting Combination Calculation...");
+            System.out.print("Enter N - ");
+            int n = sc.nextInt();
+            System.out.print("Enter R - ");
+            int r = sc.nextInt();
+            System.out.println("Equation = " + n + b + r);
+            System.out.print("Continue? (Y/N) ");
+            String c = sc.next();
+            c = c.toUpperCase();
+            char f = c.toCharArray()[0];
+            if (f == 'Y') {
+                sc.close();
+                System.out.println("Locked Input");
+                final int top = factorial(n);
+                final int bottom = factorial(n-r);
+                final int solution = top/bottom;
+                System.out.println(solution);
+            }
+        }
+        }else{
+            System.out.println("Please enter a vaild operation. Valid operation are as follows. Permutaions | Cominations");
+        }
+    }
+    public static void main(String[] args) {
+        while(true){
+        mathSolver solver = new mathSolver();
+        solver.runner();
+    }}
+
 }
