5193b7b36947fd94013e7e184880d739d7f21b42
Elliott <89421400+Elliott-cmd@users.noreply.github.com>
2022-12-11 22:25:23 -0600

    Started on part1

--- StringFirstAssignments/src/Part1.java
+++ StringFirstAssignments/src/Part1.java
@@ -1,4 +1,14 @@
 public class Part1 {
+    String findSimpleGene(String dna){
+        dna = dna.toUpperCase();
+        int sCoden = dna.indexOf("ATG");
+        int eCoden = dna.indexOf("TAA", sCoden);
+        if(sCoden != -1){
+            
+        }else{
+            return "";
+        }
+    }
     public static void main(String[] args){
         System.out.println("Hello, World!");
     }
